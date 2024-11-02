303dde4aa3436e674eba87260434322295371997
sounak9 <sounakbera9@gmail.com>
2024-11-02 09:53:36 +0530

    complete

--- project-router/src/components/Github/Github.jsx
+++ project-router/src/components/Github/Github.jsx
@@ -13,6 +13,8 @@ function Github() {
   // }, []);
   return (
     <div className=" text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      {data.name}
+      <br />
       Github followers :{data.followers}
       <img src={data.avatar_url} alt="git-picture" width={300} />
     </div>
